b2e175f344dbbdd3f252d185de9496a170db9a45
Talal Arshad <talal7860@gmail.com>
2017-11-29 01:09:34 +0500

    adding more test cases

--- test/unit/oyester.test.js
+++ test/unit/oyester.test.js
@@ -3,6 +3,7 @@ import fares from '../fixtures/fares.json';
 import stations from '../fixtures/stations.json';
 import trip1 from '../fixtures/trip1.json';
 import trip2 from '../fixtures/trip2.json';
+import trip3 from '../fixtures/trip3.json';
 import { calculateFare } from '../../src/index';
 
 describe('Fare Calculation', () => {
@@ -14,5 +15,10 @@ describe('Fare Calculation', () => {
   it('should calculate the trip from Holborn to Wimbledon as $3.00 pennies', () => {
     const fare = calculateFare(fares, stations, trip2);
     expect(fare).to.eql(300);
+  });
+
+  it('should calculate the trip from Earl\'s Court to Hammersmith as $2.00 pennies', () => {
+    const fare = calculateFare(fares, stations, trip2);
+    expect(fare).to.eql(200);
   });
 });
